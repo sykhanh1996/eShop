cdd4a779298f26d7f037928d57ae6754bf2b9a7e
DESKTOP-2M7OM6I\sykha <sykhanh1996@gmail.com>
2021-03-24 01:02:42 +0700

    structure

--- admin-app/src/app/app-routing.module.ts
+++ admin-app/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BaseComponent } from './views/layout/base/base.component';
+import { BaseComponent } from './protected-zone/layout/base/base.component';
 import { AuthGuard } from './core/guard/auth.guard';
-import { ErrorPageComponent } from './views/pages/error-page/error-page.component';
+import { ErrorPageComponent } from './protected-zone/pages/error-page/error-page.component';
 
 
 const routes: Routes = [
-  { path:'auth', loadChildren: () => import('./views/pages/auth/auth.module').then(m => m.AuthModule) },
+  { path:'auth', loadChildren: () => import('./protected-zone/pages/auth/auth.module').then(m => m.AuthModule) },
   {
     path: '',
     component: BaseComponent,
@@ -14,43 +14,43 @@ const routes: Routes = [
     children: [
       {
         path: 'dashboard',
-        loadChildren: () => import('./views/pages/dashboard/dashboard.module').then(m => m.DashboardModule)
-      },
-      {
-        path: 'apps',
-        loadChildren: () => import('./views/pages/apps/apps.module').then(m => m.AppsModule)
+        loadChildren: () => import('./protected-zone/pages/dashboard/dashboard.module').then(m => m.DashboardModule)
       },
       {
         path: 'ui-components',
-        loadChildren: () => import('./views/pages/ui-components/ui-components.module').then(m => m.UiComponentsModule)
+        loadChildren: () => import('./protected-zone/pages/ui-components/ui-components.module').then(m => m.UiComponentsModule)
       },
       {
         path: 'advanced-ui',
-        loadChildren: () => import('./views/pages/advanced-ui/advanced-ui.module').then(m => m.AdvancedUiModule)
+        loadChildren: () => import('./protected-zone/pages/advanced-ui/advanced-ui.module').then(m => m.AdvancedUiModule)
       },
       {
         path: 'form-elements',
-        loadChildren: () => import('./views/pages/form-elements/form-elements.module').then(m => m.FormElementsModule)
+        loadChildren: () => import('./protected-zone/pages/form-elements/form-elements.module').then(m => m.FormElementsModule)
       },
       {
         path: 'advanced-form-elements',
-        loadChildren: () => import('./views/pages/advanced-form-elements/advanced-form-elements.module').then(m => m.AdvancedFormElementsModule)
+        loadChildren: () => import('./protected-zone/pages/advanced-form-elements/advanced-form-elements.module').then(m => m.AdvancedFormElementsModule)
       },
       {
         path: 'charts-graphs',
-        loadChildren: () => import('./views/pages/charts-graphs/charts-graphs.module').then(m => m.ChartsGraphsModule)
+        loadChildren: () => import('./protected-zone/pages/charts-graphs/charts-graphs.module').then(m => m.ChartsGraphsModule)
       },
       {
         path: 'tables',
-        loadChildren: () => import('./views/pages/tables/tables.module').then(m => m.TablesModule)
+        loadChildren: () => import('./protected-zone/pages/tables/tables.module').then(m => m.TablesModule)
       },
       {
         path: 'icons',
-        loadChildren: () => import('./views/pages/icons/icons.module').then(m => m.IconsModule)
+        loadChildren: () => import('./protected-zone/pages/icons/icons.module').then(m => m.IconsModule)
       },
       {
         path: 'general',
-        loadChildren: () => import('./views/pages/general/general.module').then(m => m.GeneralModule)
+        loadChildren: () => import('./protected-zone/pages/general/general.module').then(m => m.GeneralModule)
+      },
+      {
+        path: 'systems',
+        loadChildren: () => import('./protected-zone/pages/general/general.module').then(m => m.GeneralModule)
       },
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, 
       // { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
